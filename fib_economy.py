046dae13ead658b3f1ac2d73b61fe46ee3a716ae
rashevskii <rashevskii@mail.ru>
2021-04-08 15:10:25 +0400

    Algorithm modified to save more memory

--- fib_economy.py
+++ fib_economy.py
@@ -1,5 +1,8 @@
-def fib_economy(number: int):
-    fib = [0, 1] + [0] * (number-1)
+def fib_economy(number: int) -> int:
+    assert number >= 0
+    # fib = [0, 1] + [0] * (number-1)  немного неэкономно по памяти
+    fib = [None] * (number + 1)  # такая реализация экономнее
+    fib[:2] = [0, 1]
     for i in range(2, number+1):
         fib[i] = fib[i - 1] + fib[i - 2]
     print(fib)
