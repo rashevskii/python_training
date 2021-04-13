488bc4bb1dcbfbfdbc93d33a7203061115bff1de
rashevskii <rashevskii@mail.ru>
2021-04-13 16:07:10 +0400

    Added algorithm of heap

---
+++ heap.py
@@ -0,0 +1,13 @@
+class Heap:
+    def __init__(self):
+        self.values = []
+        self.size = 0
+
+    def insert(self, x):
+        self.values.append(x)
+        self.size += 1
+        self.lift_up(self.size - 1)
+
+    def lift_up(self, i):
+        while i != 0 and self.values[i] < self.values[(i - 1) // 2]:
+            self.values[i], self.values[(i - 1) // 2] = self.values[(i - 1) // 2], self.values[i]
