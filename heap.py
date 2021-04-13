786ca79fa7ee6124b8f3b29c9eee227ab4de0d6f
rashevskii <rashevskii@mail.ru>
2021-04-13 16:59:39 +0400

    Added functions in algorithm of heap

--- heap.py
+++ heap.py
@@ -11,3 +11,24 @@ class Heap:
     def lift_up(self, i):
         while i != 0 and self.values[i] < self.values[(i - 1) // 2]:
             self.values[i], self.values[(i - 1) // 2] = self.values[(i - 1) // 2], self.values[i]
+
+    def extract_min(self):
+        if not self.size:
+            return None
+        tmp = self.values[0]
+        self.values[0] = self.values[-1]
+        self.values.pop()
+        self.size -= 1
+        self.lift_down(self.values[0])
+        return tmp
+
+    def lift_down(self, i):
+        while (2 * i) + 1 < self.size:
+            j = i
+            if self.values[(2 * i) + 1] < self.values[i]:
+                j = (2 * i) + 1
+            if (2 * i) + 2 < self.size and self.values[(2 * i) + 2] < self.values[j]:
+                j = (2 * i) + 2
+            if i == j:
+                break
+            self.values[i], self.values[j] = self.values[j], self.values[i]
